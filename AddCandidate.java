c782a5c6bf5c476c2e02d4474f1d37c16c7c793d
satyakalki <103808485+satyakalki@users.noreply.github.com>
2023-08-21 16:07:30 +0530

    Update AddCandidate.java

--- AddCandidate.java
+++ AddCandidate.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.SwingConstants;
 
+
 public class AddCandidate extends JPanel {
 	private JTextField candidateTextField;
 	private JTextField partyTextField;
